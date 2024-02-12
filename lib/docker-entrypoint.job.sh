eac18a41266fd333096e62f15c8bc52a34f53f29
Peter Vos <pjmvos@gmail.com>
2024-02-12 13:32:50 +0100

    split entrypoints

--- lib/docker-entrypoint.job.sh
+++ lib/docker-entrypoint.job.sh
@@ -2,12 +2,9 @@
 # https://stackoverflow.com/a/38732187/1935918
 #set -e
 
-if [ -f /usr/src/app/tmp/pids/server.pid ]; then
-  rm /usr/src/app/tmp/pids/server.pid
+if [ -f /usr/src/app/tmp/pids/delayed_job.pid ]; then
+  rm /usr/src/app/tmp/pids/delayed_job.pid
 fi
-
-bundle exec rake db:migrate
-bundle exec rake sitemap:create
 
 # Then exec the container's main process (what's set as CMD in the docker-compose.yml).
 exec bundle exec "$@"
